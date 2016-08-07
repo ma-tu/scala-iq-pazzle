03b4eef7de4e96dbb6a32a7e04a9a02d15e05a74
Kazuyuki Matsuo <matu.git@gmail.com>
2016-08-08 01:50:21 +0900

    Q1: ストリームを奇数で絞って作成するように修正

--- src/main/scala/q1/Main.scala
+++ src/main/scala/q1/Main.scala
@@ -1,18 +1,22 @@
 package q1
 
-/**
-  * Created by matu on 2016/08/07.
-  */
 object Main extends App {
+  def printTime(f: => Unit) = {
+    val start = System.currentTimeMillis()
+    f
+    println((System.currentTimeMillis - start) + " msec")
+  }
 
   def validCycleString(str: String): Boolean = str == str.reverse
 
-  val result = Stream.range(10, Int.MaxValue)
-    .find(n =>
-      validCycleString(n.toString) &&
-        validCycleString(n.toBinaryString) &&
-        validCycleString(n.toOctalString)
-    )
+  printTime {
+    val result = Stream.range(10, Int.MaxValue).filter(_ % 2 == 1)
+      .find(n =>
+        validCycleString(n.toString) &&
+          validCycleString(n.toBinaryString) &&
+          validCycleString(n.toOctalString)
+      )
 
-  result.foreach(n => println("10進数:" + n + "/2進数:" + n.toBinaryString + "/8進数:" + n.toOctalString))
+    result.foreach(n => println("10進数:" + n + "/2進数:" + n.toBinaryString + "/8進数:" + n.toOctalString))
+  }
 }
