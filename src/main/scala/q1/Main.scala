eaf979497ac455cf4aac86829a036d018c0a2348
Kazuyuki Matsuo <matu.git@gmail.com>
2016-08-08 01:13:28 +0900

    Resolved q1

--- src/main/scala/q1/Main.scala
+++ src/main/scala/q1/Main.scala
@@ -4,5 +4,15 @@ package q1
   * Created by matu on 2016/08/07.
   */
 object Main extends App {
-  println("q1 main")
+
+  def validCycleString(str: String): Boolean = str == str.reverse
+
+  val result = Stream.range(10, Int.MaxValue)
+    .find(n =>
+      validCycleString(n.toString) &&
+        validCycleString(n.toBinaryString) &&
+        validCycleString(n.toOctalString)
+    )
+
+  result.foreach(n => println("10進数:" + n + "/2進数:" + n.toBinaryString + "/8進数:" + n.toOctalString))
 }
