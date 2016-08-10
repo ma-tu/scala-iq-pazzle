36b580a48706c888ffaf2e7c67a1da2b91a8933f
Kazuyuki Matsuo <matu.git@gmail.com>
2016-08-10 21:17:59 +0900

    Resolved Q9: 速度改善（網羅方法で結果のみ取得：1987ms、加算方法：31ms)
    網羅的な探索方法をパターン数だけ算出するように修正、また格子の最短経路探索においての交点への経路パターンを加算していく方法を試した。

--- src/main/scala/q9/Main.scala
+++ src/main/scala/q9/Main.scala
@@ -1,7 +1,13 @@
 package q9
 
+import scala.collection.mutable
+
 object Main extends App {
-  util.Properties.setProp("scala.time", "")
+  def printTime(f: => Unit): Unit = {
+    val start = System.currentTimeMillis()
+    f
+    println((System.currentTimeMillis() - start) + " msec")
+  }
 
   sealed abstract class HUMAN
 
@@ -14,7 +20,7 @@ object Main extends App {
     def values = List(MAN, WOMAN)
   }
 
-  def next(maxMan: Int, maxWoman: Int, waitingMan: Int, waitingWoman: Int, sequence: List[HUMAN]): Int = {
+  def next(maxMan: Int, maxWoman: Int, waitingMan: Int, waitingWoman: Int): Int = {
     if ((maxMan - waitingMan == 0) && (maxWoman - waitingWoman == 0)) {
       1
     } else if ((waitingMan > 0) && (waitingWoman > 0) && (waitingMan == waitingWoman) || (maxMan - waitingMan == maxWoman - waitingWoman)) {
@@ -24,13 +30,13 @@ object Main extends App {
         n match {
           case HUMAN.MAN =>
             if (maxMan > waitingMan) {
-              z + next(maxMan, maxWoman, waitingMan + 1, waitingWoman, sequence :+ HUMAN.MAN)
+              z + next(maxMan, maxWoman, waitingMan + 1, waitingWoman)
             } else {
               z
             }
           case HUMAN.WOMAN =>
             if (maxWoman > waitingWoman) {
-              z + next(maxMan, maxWoman, waitingMan, waitingWoman + 1, sequence :+ HUMAN.WOMAN)
+              z + next(maxMan, maxWoman, waitingMan, waitingWoman + 1)
             } else {
               z
             }
@@ -39,6 +45,35 @@ object Main extends App {
     }
   }
 
-  val result = next(20, 10, 0, 0, List.empty)
-  println(result)
+  printTime {
+    println("網羅的に探索してみた")
+    val result = next(20, 10, 0, 0)
+    println(result)
+  }
+
+  def crossSum(x: Int, y: Int) = {
+    val sumArray: mutable.HashMap[(Int, Int), Int] = mutable.HashMap((0, 0) -> 1)
+
+    (0 to x).foreach(xx => {
+      (0 to y).foreach(yy => {
+        if ((xx != yy) && (x - xx != y - yy)) {
+          if (xx > 0) {
+            sumArray.put((xx, yy), sumArray.getOrElse((xx, yy), 0) + sumArray.getOrElse((xx - 1, yy), 0))
+          }
+          if (yy > 0) {
+            sumArray.put((xx, yy), sumArray.getOrElse((xx, yy), 0) + sumArray.getOrElse((xx, yy - 1), 0))
+          }
+        }
+      })
+    })
+
+    sumArray.toMap
+  }
+
+  printTime {
+    println("最短経路探索法で交点で加算しながら計算")
+    val result = crossSum(20, 10)
+    println(result(19, 10) + result(20, 9))
+  }
+
 }
