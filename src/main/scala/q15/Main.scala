240a84f9335926d5e4d88f73857eb006240d5b32
Kazuyuki Matsuo <matu.git@gmail.com>
2016-08-19 09:13:03 +0900

    Resolved Q15: 段数を増やしてmemo化して計測
    memo化しなかった場合は 3729ms 対応すると　148ms

--- src/main/scala/q15/Main.scala
+++ src/main/scala/q15/Main.scala
@@ -1,20 +1,28 @@
 package q15
 
+import scala.collection.mutable
+
 object Main extends App {
   util.Properties.setProp("scala.time", "")
 
+  val memo = mutable.Map.empty[(Int, Int, Int), Int]
+
   def move(a: Int, b: Int, jump: Int): Int = {
-    (a, b) match {
-      case (aa, bb) if aa == bb => 1
-      case (aa, bb) if aa > bb => 0
-      case _ =>
-        (1 to jump).map(up => {
-          (1 to jump).map(down => {
-            move(a + up, b - down, jump)
+    memo.getOrElseUpdate((a, b, jump),
+      (a, b) match {
+        case (aa, bb) if aa == bb => 1
+        case (aa, bb) if aa > bb => 0
+        case _ =>
+          (1 to jump).map(up => {
+            (1 to jump).map(down => {
+              move(a + up, b - down, jump)
+            }).sum
           }).sum
-        }).sum
-    }
+      }
+    )
   }
 
-  println(move(0, 10, 4))
+  //3777968[total 3729ms] memo化しなかった場合
+  //3777968[total 148ms] memo化した場合
+  println(move(0, 25, 4))
 }
